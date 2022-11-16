f6ab6a4cf0de9b1e461a00a8f5b402f1e689e3d7
Dubosclard Léo <leo.dubosclard@epitech.eu>
2022-11-16 09:45:22 +0100

    💄 Linter errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import { Text, Spinner, Center, VStack } from "@chakra-ui/react";
 
-const Home = (): JSX.Element => {
-  return (
-    <>
-      <Center h="100vh">
-        <VStack>
-          <Text>Let's start your development</Text>
-          <Spinner w="50px" h="50px" />
-        </VStack>
-      </Center>
-    </>
-  );
-};
+const Home = (): JSX.Element => (
+  <>
+    <Center h="100vh">
+      <VStack>
+        <Text>Let's start your development</Text>
+        <Spinner w="50px" h="50px" />
+      </VStack>
+    </Center>
+  </>
+);
 
 export default Home;
